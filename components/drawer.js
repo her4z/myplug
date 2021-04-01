0eac687eeae611d26d779bd8ecb227262ecaed1d
unknown <herassebastian@gmail.com>
2021-04-01 05:41:07 -0300

    bug fixed. Navigation to account settings screen from profile's photo was not working properly.

--- components/drawer.js
+++ components/drawer.js
@@ -9,7 +9,7 @@ import stylesDrawer from '../styles/drawer_css';
 function StyledDrawer({...props}){
     return(
         <DrawerContentScrollView {...props} test="hola">
-            <Avatar rounded size={95} source={{uri: `${Parse.User.current()?.get('image')?.url()}`}} containerStyle={stylesDrawer.avatar} onPress={()=> props.navigation.navigate('Account Settings  ')}></Avatar>
+            <Avatar rounded size={95} source={{uri: `${Parse.User.current()?.get('image')?.url()}`}} containerStyle={stylesDrawer.avatar} onPress={()=> props.navigation.navigate('Account Settings')}></Avatar>
             <Text style={stylesDrawer.textUsername}>{Parse.User.current()?.getUsername()}</Text>
             <DrawerItemList {...props}></DrawerItemList>
             <DrawerItem label="My Shopping/Trades"></DrawerItem>
